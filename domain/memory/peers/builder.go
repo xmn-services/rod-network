9a5a1a0a434e281bb5c6c1f9d052325f6fc9d6e9
Steve Rodrigue <stev.rodr@gmail.com>
2020-10-26 11:18:09 -0400

    Fixed the memory peer

--- domain/memory/peers/builder.go
+++ domain/memory/peers/builder.go
@@ -1,31 +1,16 @@
 package peers
 
 import (
-	"time"
-
 	"github.com/xmn-services/rod-network/domain/memory/peers/peer"
-	"github.com/xmn-services/rod-network/libs/entities"
-	"github.com/xmn-services/rod-network/libs/hash"
 )
 
 type builder struct {
-	hashAdapter    hash.Adapter
-	mutableBuilder entities.MutableBuilder
-	peers          []peer.Peer
-	createdOn      *time.Time
-	lastUpdatedOn  *time.Time
+	peers []peer.Peer
 }
 
-func createBuilder(
-	hashAdapter hash.Adapter,
-	mutableBuilder entities.MutableBuilder,
-) Builder {
+func createBuilder() Builder {
 	out := builder{
-		hashAdapter:    hashAdapter,
-		mutableBuilder: mutableBuilder,
-		peers:          nil,
-		createdOn:      nil,
-		lastUpdatedOn:  nil,
+		peers: nil,
 	}
 
 	return &out
@@ -33,24 +18,12 @@ func createBuilder(
 
 // Create initializes the builder
 func (app *builder) Create() Builder {
-	return createBuilder(app.hashAdapter, app.mutableBuilder)
+	return createBuilder()
 }
 
 // WithPeers add peers to the builder
 func (app *builder) WithPeers(peers []peer.Peer) Builder {
 	app.peers = peers
-	return app
-}
-
-// CreatedOn adds a creation time to the builder
-func (app *builder) CreatedOn(createdOn time.Time) Builder {
-	app.createdOn = &createdOn
-	return app
-}
-
-// LastUpdatedOn adds a lastUpdatedOn time to the builder
-func (app *builder) LastUpdatedOn(lastUpdatedOn time.Time) Builder {
-	app.lastUpdatedOn = &lastUpdatedOn
 	return app
 }
 
@@ -60,23 +33,5 @@ func (app *builder) Now() (Peers, error) {
 		app.peers = []peer.Peer{}
 	}
 
-	data := [][]byte{
-		[]byte("init"),
-	}
-
-	for _, onePeer := range app.peers {
-		data = append(data, onePeer.Hash().Bytes())
-	}
-
-	hsh, err := app.hashAdapter.FromMultiBytes(data)
-	if err != nil {
-		return nil, err
-	}
-
-	mutable, err := app.mutableBuilder.Create().WithHash(*hsh).CreatedOn(app.createdOn).LastUpdatedOn(app.lastUpdatedOn).Now()
-	if err != nil {
-		return nil, err
-	}
-
-	return createPeers(mutable, app.peers), nil
+	return createPeers(app.peers), nil
 }
--- domain/memory/peers/peers.go
+++ domain/memory/peers/peers.go
@@ -1,46 +1,24 @@
 package peers
 
 import (
-	"time"
-
 	"github.com/xmn-services/rod-network/domain/memory/peers/peer"
-	"github.com/xmn-services/rod-network/libs/entities"
-	"github.com/xmn-services/rod-network/libs/hash"
 )
 
 type peers struct {
-	mutable entities.Mutable
-	peers   []peer.Peer
+	peers []peer.Peer
 }
 
 func createPeers(
-	mutable entities.Mutable,
 	lst []peer.Peer,
 ) Peers {
 	out := peers{
-		mutable: mutable,
-		peers:   lst,
+		peers: lst,
 	}
 
 	return &out
 }
 
-// Hash returns the hash
-func (obj *peers) Hash() hash.Hash {
-	return obj.mutable.Hash()
-}
-
 // All returns the peers
 func (obj *peers) All() []peer.Peer {
 	return obj.peers
-}
-
-// LastUpdatedOn returns the lastUpdatedOn time
-func (obj *peers) LastUpdatedOn() time.Time {
-	return obj.mutable.LastUpdatedOn()
-}
-
-// CreatedOn returns the creation time
-func (obj *peers) CreatedOn() time.Time {
-	return obj.mutable.CreatedOn()
 }
